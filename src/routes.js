4f5a47d29c1864dc6963ec9e9368d2f549e299e3
Yaoshaungfei <18767176093@163.com>
2017-12-25 18:02:16 +0800

    nav

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,154 @@ import Home from './views/Home.vue'
 import Main from './views/Home/indexMain.vue'
 import { state } from './vuex/state'
 
+import DeliverGoods from './views/logistics/DeliverGoods.vue'
+import StoreInformation from './views/Home/StoreInformation.vue'
+
+
+
+
+// 产品管理
+    import xinshou from './views/aa_product_management/xinshou.vue'
+    import routine from './views/aa_product_management/routine.vue'
+    import yuyue from './views/aa_product_management/yuyue.vue'
+
+    import producta from './views/aa_product_management/producta.vue'
+    import productb from './views/aa_product_management/productb.vue'
+    import productc from './views/aa_product_management/productc.vue'
+    import productd from './views/aa_product_management/productd.vue'
+    import producte from './views/aa_product_management/producte.vue'
+    import productf from './views/aa_product_management/productf.vue'
+
+    import interest from './views/aa_product_management/interest.vue'
+    import interesta from './views/aa_product_management/interesta.vue'
+
+// 运营管理
+    import register from './views/aa_operation_management/register.vue'
+    import registera from './views/aa_operation_management/registera.vue'
+    import registerb from './views/aa_operation_management/registerb.vue'
+    import registerc from './views/aa_operation_management/registerc.vue'
+    import registerd from './views/aa_operation_management/registerd.vue'
+
+    import fbbanner from './views/aa_operation_management/fbbanner.vue'
+    import fbbannera from './views/aa_operation_management/fbbannera.vue'
+
+    import edition from './views/aa_operation_management/edition.vue'
+    import editiona from './views/aa_operation_management/editiona.vue'
+    import editionb from './views/aa_operation_management/editionb.vue'
+    import editionc from './views/aa_operation_management/editionc.vue'
+
+    import journalism from './views/aa_operation_management/journalism.vue'
+    import journalisma from './views/aa_operation_management/journalisma.vue'
+    import journalismb from './views/aa_operation_management/journalismb.vue'
+    import journalismc from './views/aa_operation_management/journalismc.vue'
+
+    import form from './views/aa_operation_management/form.vue'
+    import forma from './views/aa_operation_management/forma.vue'
+    import formb from './views/aa_operation_management/formb.vue'
+    import formc from './views/aa_operation_management/formc.vue'
+    import formd from './views/aa_operation_management/formd.vue'
+    import forme from './views/aa_operation_management/forme.vue'
+    import formf from './views/aa_operation_management/formf.vue'
+    import formg from './views/aa_operation_management/formg.vue'
+    import formh from './views/aa_operation_management/formh.vue'
+    import formi from './views/aa_operation_management/formi.vue'
+    import formj from './views/aa_operation_management/formj.vue'
+    import formk from './views/aa_operation_management/formk.vue'
+    import forml from './views/aa_operation_management/forml.vue'
+    import formm from './views/aa_operation_management/formm.vue'
+    import formn from './views/aa_operation_management/formn.vue'
+    import formo from './views/aa_operation_management/formo.vue'
+    
+    import data from './views/aa_operation_management/data.vue'
+    import datab from './views/aa_operation_management/datab.vue'
+    import datac from './views/aa_operation_management/datac.vue'
+    import datad from './views/aa_operation_management/datad.vue'
+    import datae from './views/aa_operation_management/datae.vue'
+    import dataf from './views/aa_operation_management/dataf.vue'
+    import datag from './views/aa_operation_management/datag.vue'
+    import datah from './views/aa_operation_management/datah.vue'
+    import datam from './views/aa_operation_management/datam.vue'
+    import datan from './views/aa_operation_management/datan.vue'
+
+// 活动管理
+    import activity from './views/aa_activity_management/activity.vue'
+
+// 财务管理
+    import financia from './views/aa_finance_management/financia.vue'
+    import financiaa from './views/aa_finance_management/financiaa.vue'
+    import financiab from './views/aa_finance_management/financiab.vue'
+    import financiac from './views/aa_finance_management/financiac.vue'
+
+    import recharge from './views/aa_finance_management/recharge.vue'
+    import rechargea from './views/aa_finance_management/rechargea.vue'
+    import rechargeb from './views/aa_finance_management/rechargeb.vue'
+    import rechargec from './views/aa_finance_management/rechargec.vue'
+
+    import withdrawals from './views/aa_finance_management/withdrawals.vue'
+    import withdrawalsa from './views/aa_finance_management/withdrawalsa.vue'
+    import withdrawalsb from './views/aa_finance_management/withdrawalsb.vue'
+    import withdrawalsc from './views/aa_finance_management/withdrawalsc.vue'
+    import withdrawalsd from './views/aa_finance_management/withdrawalsd.vue'
+    import withdrawalse from './views/aa_finance_management/withdrawalse.vue'
+    import withdrawalsf from './views/aa_finance_management/withdrawalsf.vue'
+
+    import loan from './views/aa_finance_management/loan.vue'
+
+
+// 权限管理 
+    import jurisdictiona from './views/aa_jurisdiction_management/jurisdictiona.vue'
+    import jurisdictionb from './views/aa_jurisdiction_management/jurisdictionb.vue'
+    import jurisdictionc from './views/aa_jurisdiction_management/jurisdictionc.vue'
+    import jurisdictiond from './views/aa_jurisdiction_management/jurisdictiond.vue'
+    import jurisdictione from './views/aa_jurisdiction_management/jurisdictione.vue'
+    import jurisdictionf from './views/aa_jurisdiction_management/jurisdictionf.vue'
+    import jurisdictiong from './views/aa_jurisdiction_management/jurisdictiong.vue'
+    import jurisdictionh from './views/aa_jurisdiction_management/jurisdictionh.vue'
+
+// 功能管理
+    import thread from './views/aa_function_management/thread.vue'
+
+    import unbundlinga from './views/aa_function_management/unbundlinga.vue'
+    import unbundlingb from './views/aa_function_management/unbundlingb.vue'
+
+    import change from './views/aa_function_management/change.vue'
+    import changea from './views/aa_function_management/changea.vue'
+
+    import smsa from './views/aa_function_management/smsa.vue'
+    import smsb from './views/aa_function_management/smsb.vue'
+    import smsc from './views/aa_function_management/smsc.vue'
+
+    import vouchera from './views/aa_function_management/vouchera.vue'
+    import voucherb from './views/aa_function_management/voucherb.vue'
+    import voucherc from './views/aa_function_management/voucherc.vue'
+    import voucherd from './views/aa_function_management/voucherd.vue'
+    import vouchere from './views/aa_function_management/vouchere.vue'
+
+    import blacklist from './views/aa_function_management/blacklist.vue'
+    import blacklista from './views/aa_function_management/blacklista.vue'
+
+// 模块管理
+    import coin_pursea from './views/aa_modular_management/coin_pursea.vue'
+    import coin_purseb from './views/aa_modular_management/coin_purseb.vue'
+    import coin_pursec from './views/aa_modular_management/coin_pursec.vue'
+    import coin_pursed from './views/aa_modular_management/coin_pursed.vue'
+    import coin_pursee from './views/aa_modular_management/coin_pursee.vue'
+    import coin_pursef from './views/aa_modular_management/coin_pursef.vue'
+
+    import invitinga from './views/aa_modular_management/invitinga.vue'
+    import invitingb from './views/aa_modular_management/invitingb.vue'
+
+// 历史下载
+    import history from './views/aa_history_management/history.vue'
+    import historya from './views/aa_history_management/historya.vue'
+    import historyb from './views/aa_history_management/historyb.vue'
+    import historyc from './views/aa_history_management/historyc.vue'
+
+// 系统管理
+    import system from './views/aa_system_management/system.vue'
+    import systema from './views/aa_system_management/systema.vue'
+    import systemb from './views/aa_system_management/system.vue'
+
 
 
 
@@ -53,8 +201,6 @@ import already_bad_debts from './views/after_loan/already_bad_debts.vue'
 
 
 
-// 店铺信息
-import StoreInformation from './views/Home/StoreInformation.vue'
 
 /*系统管理 systemSettings*/  
 import Jurisdiction from './views/systemSettings/Jurisdiction.vue'
@@ -66,7 +212,7 @@ import releaseReg from './views/systemSettings/releaseReg.vue'
 
 /*配置管理 configuration*/
 import banner from './views/configuration/banner.vue' 
-import system from './views/configuration/system.vue' 
+// import system from './views/configuration/system.vue' 
 import Authentication from './views/configuration/Authentication.vue'
 
 
@@ -77,7 +223,6 @@ import OrderInformation from './views/order/OrderInformation.vue'
 import details from './views/order/details.vue'
 
 // 物流管理 logistics
-import DeliverGoods from './views/logistics/DeliverGoods.vue'
 import Freight from './views/logistics/Freight.vue'
 // import Address from './views/logistics/Address.vue'
 
@@ -147,29 +292,112 @@ let routes = [
             { path: '/StoreInformation', component: StoreInformation, name: '首页' ,  hidden: true  }
         ]
     },
-    
-    // 
-    // 
-    // 
-    // 
     {
         path: '/',
         component: Home,
-        name: ' 商户管理',
+        name: ' 产品管理',
         iconCls: 'el-icon-message',//图标样式class
         children: [
             // { path: '/main', component:Main, name: '主页', hidden: true },
-            {  path: '/', component:DeliverGoods, name: '商户信息' ,children:[
+            {  path: '/', component:DeliverGoods, name: '产品发布' ,children:[
+                { path: '/xinshou', component:xinshou, name: '发布新手产品' },
+                { path: '/routine', component:routine, name: '发布常规产品' },
+                { path: '/yuyue', component:yuyue, name: '发布预约产品' }
+            ]},
+            {  path: '/', component:DeliverGoods,name: '产品信息' ,children:[
+                { path: '/producta', component: producta, name: '产品发布记录' },
+                { path: '/productb', component: productb, name: '查看预约产品' },
+                { path: '/productc', component: productc, name: '产品统计' },
+                { path: '/productd', component: productd, name: '在售产品' },
+                { path: '/producte', component: producte, name: '投资记录' },
+                { path: '/productf', component: productf, name: '虚拟投资列表' }
+            ]},
+            { path: '/', component:DeliverGoods, name: '产品信息' ,children:[
+                { path: '/interest', component: interest, name: '付息总表' },
+                { path: '/interesta', component: interesta, name: '付息明细表' }
+            ]}
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: ' 运营管理',
+        iconCls: 'el-icon-message',//图标样式class
+        children: [
+            // { path: '/main', component:Main, name: '主页', hidden: true },
+            {  path: '/', component:DeliverGoods, name: '注册管理' ,children:[
+                { path: '/register', component:register, name: '商户借款' },
+                { path: '/registera', component:registera, name: '商户反馈列表' },
+                { path: '/registerb', component:registerb, name: '商户反馈列表' },
+                { path: '/registerc', component:registerc, name: '商户反馈列表' },
+                { path: '/registerd', component:registerd, name: '商户借款记录' }
+            ]},
+            {  path: '/', component:DeliverGoods,name: 'Banner管理' ,children:[
+                { path: '/fbbanner', component: fbbanner, name: '已还款订单' },
+                { path: '/fbbannera', component: fbbannera, name: '已坏账订单' },
+            ]},
+            { path: '/', component:DeliverGoods, name: '版本管理' ,children:[
+                { path: '/edition', component: edition, name: '发布商品' },
+                { path: '/editiona', component: editiona, name: '出售中的商品' },
+                { path: '/editionb', component: editionb, name: '下架商品' },
+                { path: '/editionc', component: editionc, name: '退货的商品' }
+            ]},
+            {  path: '/', component:DeliverGoods, name: '新闻公告' ,children:[
+                { path: '/journalism', component:journalism, name: '商户借款' },
+                { path: '/journalisma', component:journalisma, name: '商户反馈列表' },
+                { path: '/journalismb', component:journalismb, name: '商户反馈列表' },
+                { path: '/journalismc', component:journalismc, name: '商户借款记录' }
+            ]},
+            {  path: '/', component:DeliverGoods,name: '报表统计' ,children:[
+                { path: '/form', component: form, name: '安卓聚到汇总' },
+                { path: '/forma', component: forma, name: '渠道投资计划(新)' },
+                { path: '/formb', component: formb, name: '平台渠道统计' },
+                { path: '/formc', component: formc, name: 'IOS渠道统计汇总' },
+                { path: '/formd', component: formd, name: '渠道成本' },
+                { path: '/formd', component: formd, name: '每日投资统计' },
+                { path: '/forme', component: forme, name: '平台投资情况' },
+                { path: '/formf', component: formf, name: '数据统计' },
+                { path: '/formg', component: formg, name: '用户转换数据' },
+                { path: '/formh', component: formh, name: '充值提现数据报表' },
+                { path: '/formi', component: formi, name: '运营数据' },
+                { path: '/formj', component: formj, name: '渠道关键字查询' },
+                { path: '/formk', component: formk, name: '注册投资充值提现' },
+                { path: '/forml', component: forml, name: '用户投资统计' },
+                { path: '/formm', component: formm, name: 'wap用户投资统计' },
+                { path: '/formn', component: formn, name: '访客渠道投资统计' }
+            ]},
+            { path: '/', component:DeliverGoods, name: '数据分析' ,children:[
+                { path: '/data', component: data, name: '地域统计' },
+                { path: '/datab', component: datab, name: '用户数据分析' },
+                { path: '/datac', component: datac, name: '用户年龄分布表' },
+                { path: '/datad', component: datad, name: '用户评论' },
+                { path: '/datae', component: datae, name: '用户投资记录分析' },
+                { path: '/dataf', component: dataf, name: '金额排行榜' },
+                { path: '/datag', component: datag, name: '性别统计' },
+                { path: '/datah', component: datah, name: '投资分析曲线' },
+                { path: '/datam', component: datam, name: '银行卡数据统计' },
+                { path: '/datan', component: datan, name: '绑卡用户信息统计' }
+            ]}
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: ' 产品管理',
+        iconCls: 'el-icon-message',//图标样式class
+        children: [
+            // { path: '/main', component:Main, name: '主页', hidden: true },
+            {  path: '/', component:DeliverGoods, name: '产品发布' ,children:[
                 { path: '/business_info', component:business_info, name: '商户借款' },
                 { path: '/business_feedback', component:business_feedback, name: '商户反馈列表' },
                 { path: '/business_loan', component:business_loan, name: '商户借款记录' }
             ]},
-            {  path: '/', component:DeliverGoods,name: '商户信息' ,children:[
+            {  path: '/', component:DeliverGoods,name: '产品信息' ,children:[
                 { path: '/reimbursement', component: reimbursement, name: '已还款订单' },
                 { path: '/overdue', component: overdue, name: '已逾期订单' },
                 { path: '/already_bad_debts', component: already_bad_debts, name: '已坏账订单' },
             ]},
-            { path: '/', component:DeliverGoods, name: '商户信息' ,children:[
+            { path: '/', component:DeliverGoods, name: '产品信息' ,children:[
                 { path: '/GoodShipped', component: GoodShipped, name: '发布商品' },
                 { path: '/OnSale', component: OnSale, name: '出售中的商品' },
                 { path: '/OffTheShelf', component: OffTheShelf, name: '下架商品' },
